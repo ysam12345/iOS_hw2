ea5db125a618e974ce894a6f5505e9ca62dd3a4b
游子謙 <ysam12345@gmail.com>
2018-12-05 11:12:22 +0800

    Initial Commit

---
+++ iOS-hw2-questionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-hw2-questionApp
+//
+//  Created by yochien on 2018/12/5.
+//  Copyright © 2018 yochien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
